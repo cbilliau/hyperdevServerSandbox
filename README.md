d004702fa8639fa4044579c067b1df5cb68093ae
Chris Billiau <cbilliau@users.noreply.github.com>
2016-11-26 11:40:20 -0500

    👹🍐 Updated with HyperDev

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# hyperdevServerSandbox
-repo for HyperDev node server sandbox
+Node Server Work
+=================
+
+Node Server learn
+
+[Anatomy of an HTTP Transaction in Node](https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/)
+
+
+Project
+--------
+
+
---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  request.on('error', function(err) {
+    console.error(err);
+  }).on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
+    // At this point, we have the headers, method, url and body, and can now
+    // do whatever we need to in order to respond to this request.
+  });
+  
+}).listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + process.env.PORT);
+});
