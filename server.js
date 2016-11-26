32bb45562669e6da20ad1225c28845beffd99183
Chris Billiau <cbilliau@users.noreply.github.com>
2016-11-26 12:09:38 -0500

    request and response

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
-var http = require('http');
+const http = require('http');
 
-http.createServer(function(request, response) {
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
-  var body = [];
-  request.on('error', function(err) {
+http.createServer((request, response) => {
+  let headers = request.headers;
+  let method = request.method;
+  let url = request.url;
+  let body = [];
+  
+  request.on('error', (err) => {
     console.error(err);
-  }).on('data', function(chunk) {
+  }).on('data', (chunk) => {
     body.push(chunk);
-  }).on('end', function() {
+  }).on('end', () => {
     body = Buffer.concat(body).toString();
-    // At this point, we have the headers, method, url and body, and can now
-    // do whatever we need to in order to respond to this request.
+    // At this point, we have the headers, method, url and body.
   });
+  
+  response.on('error', (err) => {
+    console.error(err);
+  });
+  
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'application/json');
+  // or response.writeHead(200, {'Content-Type': 'application/json'})
+  
+  let responseBody = {
+    headers: headers,
+    method: method,
+    url: url,
+    body: body
+  };
+ 
+  response.write(JSON.stringify(responseBody));
+  response.end();
+  // or response.end(JSON.stringify(responseBody))
   
 }).listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + process.env.PORT);
